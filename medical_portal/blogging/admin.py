d665efda38e34a3658aebafcc173b06f19c522e3
Rahul Thorat <137529661+coderRahul2604@users.noreply.github.com>
2024-08-08 22:07:56 +0530

    Delete medical_portal directory

--- medical_portal/blogging/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from blogging.models import Category, Blog
-
-# Register your models here
-
-admin.site.register(Category)
-admin.site.register(Blog)
--- medical_portal/blogging/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Blog, Category
-
-class BlogForm(forms.ModelForm):
-    class Meta:
-        model = Blog
-        fields = ['title', 'image', 'category', 'summary', 'content', 'is_draft']
